6035f6e8a0661415e0acaa9eea18505a3f177cf6
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2020-05-18 20:38:39 +0200

    WIP * Added C# client

---
+++ client_csharp/Helpers/GrpcHelper.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using SeismoSrv;
+
+namespace gRPC.Helpers
+{
+    class GrpcHelper
+    {
+        public string ServerAddress { get; private set; }
+
+        public GrpcHelper(string serverAddress)
+        {
+            ServerAddress = serverAddress;
+        }
+
+        public string PingServer()
+        {
+            var channel = GrpcChannel.ForAddress(ServerAddress);
+            var client = new SeismoService.SeismoServiceClient(channel);
+            var reply = client.SeismoPing(new SeismoPingRequest() { Name = "C# Client" });
+            return reply.Message;
+        }
+    }
+}
---
+++ client_csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using gRPC.Helpers;
+using System;
+
+namespace gRPC
+{
+    class Program
+    {
+        private static string _srvAddress = "127.0.0.1:50051";
+
+        static void Main(string[] args)
+        {
+            var _grpcHelper = new GrpcHelper(_srvAddress);
+            var _pingMsg = _grpcHelper.PingServer();
+            Console.WriteLine(_pingMsg);
+        }
+    }
+}
